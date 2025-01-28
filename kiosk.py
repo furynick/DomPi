b8cfdce36847b995bf2b7dc1200f036b2b3d82ed
Nicolas T <furydonnico@gmail.com>
2025-01-28 13:23:24 +0100

    Add power ratings with animation, manage JSON exception

--- kiosk.py
+++ kiosk.py
@@ -28,23 +28,27 @@ class TactileZone:
 locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
 
 # Global variables
-tgt_arch  = "aarch64"
-bt_addr   = "60:6D:C7:70:A6:34"
-first_run = True
-running   = True
-boiler    = False
-solar_pw  = 0.0
-grid_pw   = 0.0
-batt_pw   = 0.0
-batt_max  = 5000.0
-grid_max  = 6800.0
-sol_max   = 2920.0
-tempo_now = 'UNKN'
-tempo_tmw = 'UNKN'
-cur_batt  = '0,0%'
-cur_temp  = '19.0°'
-cur_hum   = '45%'
-gauge_h   = 80
+tgt_arch   = "aarch64"
+bt_addr    = "60:6D:C7:70:A6:34"
+first_run  = True
+running    = True
+boiler     = False
+info       = False
+animate    = False
+solar_pw   = 0.0
+grid_pw    = 0.0
+batt_pw    = 0.0
+batt_max   = 5000.0
+grid_max   = 6800.0
+sol_max    = 2920.0
+tempo_now  = 'UNKN'
+tempo_tmw  = 'UNKN'
+cur_batt   = '0,0%'
+cur_temp   = '19.0°'
+cur_hum    = '45%'
+gauge_h    = 80
+anim_pct   = 0
+anim_dly   = 7
 
 # Temperature sensor setup
 i2c = board.I2C()  # uses board.SCL and board.SDA
@@ -101,6 +105,8 @@ TEMPO_TICK = pygame.event.custom_type()
 TEMP_TICK  = pygame.event.custom_type()
 MQTT_TICK  = pygame.event.custom_type()
 BOILER_OFF = pygame.event.custom_type()
+INFO_OFF   = pygame.event.custom_type()
+ANIMATE    = pygame.event.custom_type()
 pygame.time.set_timer(TEMPO_TICK, 120000)
 pygame.time.set_timer(MQTT_TICK,   30000)
 pygame.time.set_timer(TEMP_TICK,    3000)
@@ -133,17 +139,25 @@ def on_message(client, userdata, message, properties=None):
     global grid_pw
     global batt_pw
 
-    payload = json.loads(message.payload)
-    match message.topic:
-        case "N/d83add91edfc/battery/291/Soc":
-            buf = "%0.1f%%" % float(payload['value'])
-            cur_batt = buf.replace(".", ",")
-        case "N/d83add91edfc/battery/291/Dc/0/Power":
-            batt_pw = float(payload['value'])
-        case "N/d83add91edfc/grid/30/Ac/Power":
-            grid_pw = float(payload['value'])
-        case "N/d83add91edfc/pvinverter/20/Ac/Power":
-            solar_pw = float(payload['value'])
+    try:
+        payload = json.loads(message.payload)
+    except json.decoder.JSONDecodeError as e:
+        print("Error decoding JSON", message)
+    except ValueError as e:
+        print("Error decoding", message)
+    except TypeError as e:
+        print("Invalid type", message)
+    else:
+        match message.topic:
+            case "N/d83add91edfc/battery/291/Soc":
+                buf = "%0.1f%%" % float(payload['value'])
+                cur_batt = buf.replace(".", ",")
+            case "N/d83add91edfc/battery/291/Dc/0/Power":
+                batt_pw = float(payload['value'])
+            case "N/d83add91edfc/grid/30/Ac/Power":
+                grid_pw = float(payload['value'])
+            case "N/d83add91edfc/pvinverter/20/Ac/Power":
+                solar_pw = float(payload['value'])
 
 def on_subscribe(client, userdata, mid, qos, properties=None):
     print(f"{datetime.now()} Subscribed with QoS {qos}")
@@ -187,12 +201,14 @@ def gaugeDrawLine(x, y, n, c):
     pygame.draw.line(screen, c, (x, y), (x + l, y))
 
 def gaugeDraw(base, pct, col, rev = False):
-    count = int(gauge_h * pct / 100)
+    min_h = 5
+    height = int(((gauge_h - min_h) * (100 - anim_pct) / 100) + min_h)
+    count = int(height * pct / 100)
     n = 0
     
-    pygame.draw.rect(screen, fground_col, (765, base - gauge_h - 2, 23, gauge_h + 6), 0, 8)
+    pygame.draw.rect(screen, fground_col, (765, base - height - 2, 23, height + 6), 0, 8)
     if rev:
-        for y in range(base - gauge_h, base - gauge_h + count):
+        for y in range(base - height, base - height + count):
             n += 1
             gaugeDrawLine(767, y, n, col)
     else:
@@ -212,12 +228,44 @@ def buildMainUI():
     global solar_pw
     global tactile_zones
     global boiler
+    global info
+    global anim_pct
     global first_run
 
     cur_time = strftime('%H:%M')
     cur_date = strftime('%A ') + strftime('%d').lstrip('0') + strftime(' %B %Y')
 
+#    for zone in tactile_zones:
+#        pygame.draw.rect(screen, fground_col, zone.rect, 1, 10)
+
     screen.blit(background, (0, 0))
+
+    tempoDraw(tempo_now, (863, 63), 17)
+    tempoDraw(tempo_tmw, (900, 67), 13)
+    r = pygame.Rect(699 - anim_pct, 112, 269 + anim_pct, 432)
+    pygame.draw.rect(screen, fground_col, r, 2, 33)
+    if first_run:
+      tactile_zones.append(TactileZone(click, r, "info"))
+    if anim_pct != 100:
+        gaugeDraw(216, int(100.0 * solar_pw / sol_max), solar_col)
+        if grid_pw > 0.0:
+            gaugeDraw(327, int( 100.0 * grid_pw / grid_max), grid_fw_col)
+        else:
+            gaugeDraw(327, int(-100.0 * grid_pw / grid_max), grid_bw_col, True)
+        if batt_pw > 0.0:
+            gaugeDraw(438, int( 100.0 * batt_pw / batt_max), bat_chg_col)
+        else:
+            gaugeDraw(438, int(-100.0 * batt_pw / batt_max), bat_dch_col, True)
+    else:
+        pass
+        y = 180
+        for watt in [ solar_pw, grid_pw, batt_pw ]:
+            text_srf = font_batt.render("%dW" % watt, True, fground_col, None)
+            text_crd = text_srf.get_rect()
+            text_crd.midright = (805, y)
+            screen.blit(text_srf, text_crd)
+            y += 111
+
     date_srf = font_date.render(cur_date, True, fground_col, None)
     date_crd = date_srf.get_rect()
     date_crd.center = (295, 150)
@@ -242,22 +290,6 @@ def buildMainUI():
     text_crd = text_srf.get_rect()
     text_crd.center = (788,   65)
 
-#    for zone in tactile_zones:
-#        pygame.draw.rect(screen, fground_col, zone.rect, 1, 10)
-
-    tempoDraw(tempo_now, (863, 63), 17)
-    tempoDraw(tempo_tmw, (900, 67), 13)
-    pygame.draw.rect(screen, fground_col, (692, 112, 269, 432), 2, 33)
-    gaugeDraw(216, int(100.0 * solar_pw / sol_max), solar_col)
-    if grid_pw > 0.0:
-        gaugeDraw(327, int( 100.0 * grid_pw / grid_max), grid_fw_col)
-    else:
-        gaugeDraw(327, int(-100.0 * grid_pw / grid_max), grid_bw_col, True)
-    if batt_pw > 0.0:
-        gaugeDraw(438, int( 100.0 * batt_pw / batt_max), bat_chg_col)
-    else:
-        gaugeDraw(438, int(-100.0 * batt_pw / batt_max), bat_dch_col, True)
-
     screen.blit(hour_srf, hour_crd)
     screen.blit(date_srf, date_crd)
     screen.blit(temp_srf, temp_crd)
@@ -268,15 +300,9 @@ def buildMainUI():
         screen.blit(blue_flame, (275, 50))
     else:
         screen.blit(grey_flame, (275, 50))
-    r = screen.blit(icon_solar,     (830, 138))
-    if first_run:
-      tactile_zones.append(TactileZone(click, r, "solar"))
-    r = screen.blit(icon_grid,      (830, 251))
-    if first_run:
-      tactile_zones.append(TactileZone(click, r, "grid"))
-    r = screen.blit(icon_battery,   (830, 361))
-    if first_run:
-      tactile_zones.append(TactileZone(click, r, "battery"))
+    screen.blit(icon_solar,     (830, 138))
+    screen.blit(icon_grid,      (830, 251))
+    screen.blit(icon_battery,   (830, 361))
     r = screen.blit(play_disabled , (225, 453))
     if first_run:
       tactile_zones.append(TactileZone(click, r, "play"))
@@ -309,9 +335,10 @@ def signal_handler(sig, frame):
 
 def click(duration_ms, name):
     global boiler
+    global info
 
     if duration_ms > 1000: # very long press
-        print("Short click on", name)
+        print("Very long click on", name)
     elif duration_ms > 150: # long press
         print("Long click on", name)
         match name:
@@ -322,7 +349,13 @@ def click(duration_ms, name):
                     boiler = True
                     pygame.time.set_timer(BOILER_OFF, 20*60000, 1)
     else: # short press
-        pass
+        print("Short click on", name)
+        match name:
+            case "info":
+                if not info:
+                    info = True
+                    pygame.time.set_timer(INFO_OFF, 30000,  1)
+                    pygame.time.set_timer(ANIMATE, anim_dly, 100)
 
 tactile_zones = []
 tactile_zones.append(TactileZone(click, pygame.Rect(270,  35,  48,  70), "boiler"))
@@ -361,6 +394,14 @@ while running:
             cur_hum  = "%d%%" % int(sensor.relative_humidity)
         elif event.type == BOILER_OFF:
             boiler = False
+        elif event.type == INFO_OFF:
+            info = False
+            pygame.time.set_timer(ANIMATE, anim_dly, 100)
+        elif event.type == ANIMATE:
+            if info:
+                anim_pct += 1
+            else:
+                anim_pct -= 1
         else:
             print("Unknown %d", event.type)
 
