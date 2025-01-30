0ae7e1dc9e53599da9c398bd9d343c3430cf00de
Nicolas T <furydonnico@gmail.com>
2025-01-30 14:54:53 +0100

    Player test successful

--- teststream.py
+++ teststream.py
@@ -1,8 +1,56 @@
 import vlc
+import yt_dlp
+import sys
 
-test_url = 'https://rr2---sn-25ge7nzz.googlevideo.com/videoplayback?expire=1738244335&ei=jyybZ9KJKOnOp-oP-_P0yAI&ip=82.64.198.215&id=o-AE_6Y4eys6JB77HR4svi6X-6tlfZRj9q5V8yazvKIzWZ&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1738222735%2C&mh=X-&mm=31%2C29&mn=sn-25ge7nzz%2Csn-25glenlz&ms=au%2Crdu&mv=m&mvi=2&pl=20&rms=au%2Cau&gcr=fr&initcwndbps=3040000&bui=AY2Et-M-6tzWNSeGuSg8CgM7xvCmj9d-7iT0Q1_fJAr0KnJflnmYWFcAc4CrAEsmB4r2VXmI3ubO4xZd&spc=9kzgDboXWImLKddzZH0umwh_psmyyflNbD0Eovm7lqNbq48QN11Uxjccew&vprv=1&svpuc=1&mime=audio%2Fwebm&ns=WY73ENrlX05J0dvCAQ7OuWkQ&rqh=1&gir=yes&clen=3786183&dur=222.801&lmt=1714513186152329&mt=1738222371&fvip=3&keepalive=yes&lmw=1&fexp=51326932%2C51371294&c=TVHTML5&sefc=1&txp=5432434&n=KBs5jXbMH63BMQ&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Crms%2Cinitcwndbps&lsig=AGluJ3MwRQIhAM03CQHtM4YEMUPVhpu2SeL2-vzAXJRVKzR8yN1IjRYcAiB7XsDxk-OT6RnrERvmSNd545Fe9ul20k8cMpWU8bgAdQ%3D%3D&sig=AJfQdSswRAIgUH9edDU4xFzLo7t_Rdd_aCAAXUBXeAwiyokT7s8MxLICID2HSNEljItwbh_vinT23lFoeHwLRGeyJzM-lV7SAsEZ'
+def pprint(web, level=0):
+    for k,v in web.items():
+        if isinstance(v, dict):
+            print('\t'*level, f'{k}: ')
+            level += 1
+            pprint(v, level)
+            level -= 1
+        else:
+            print('\t'*level, k, ": ", v)
 
-p = vlc.MediaPlayer(test_url)
+def dump(obj, nested_level=0, output=sys.stdout):
+    spacing = '   '
+    if isinstance(obj, dict):
+        print >> output, '%s{' % ((nested_level) * spacing)
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print >> output, '%s%s:' % ((nested_level + 1) * spacing, k)
+                dump(v, nested_level + 1, output)
+            else:
+                print >> output, '%s%s: %s' % ((nested_level + 1) * spacing, k, v)
+        print >> output, '%s}' % (nested_level * spacing)
+    elif isinstance(obj, list):
+        print >> output, '%s[' % ((nested_level) * spacing)
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dump(v, nested_level + 1, output)
+            else:
+                print >> output, '%s%s' % ((nested_level + 1) * spacing, v)
+        print >> output, '%s]' % ((nested_level) * spacing)
+    else:
+        print >> output, '%s%s' % (nested_level * spacing, obj)
+
+yt_url   = 'https://www.youtube.com/watch?v=bXr-9tWhefo'
+ydl_opts = {"geturl": True, "quiet": True}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info(yt_url, download=False)
+print(info["title"])
+size = 8192
+for thumbnail in info["thumbnails"]:
+    if thumbnail["height"] != thumbnail["width"]:
+        continue
+    if thumbnail["height"] >= size:
+        continue
+    size    = thumbnail["height"]
+    art_url = thumbnail["url"]
+print(art_url)
+
+p = vlc.MediaPlayer(info["requested_formats"][1]["url"])
 p.play()
 input("press enter to quit")
 p.stop()
