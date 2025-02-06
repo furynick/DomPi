f23e991560182c6a9ad3973a27b9b174935126be
Nicolas Tuffier <nicolas.tuffier.ext@corp.ovh.com>
2025-02-06 17:58:56 +0100

    GPIO rework, prepare sched page

--- kiosk.py
+++ kiosk.py
@@ -13,7 +13,6 @@ import platform
 import threading
 import subprocess
 import pygame.gfxdraw
-import RPi.GPIO as GPIO
 import subprocess as sp
 import paho.mqtt.client as paho
 from io import BytesIO
@@ -29,21 +28,10 @@ from collections.abc import Callable
 from paho.mqtt.properties import Properties
 from paho.mqtt.packettypes import PacketTypes 
 
-# ✅ Initialisation de YTMusic avec OAuth
-ytmusic = YTMusic('.priv/oauth.json')
+rpi = platform.machine() == "aarch64"
+if rpi: import RPi.GPIO as GPIO
 
-# ✅ Files tampons
-q_playlist = queue.Queue()               # IDs des playlists
-q_track = queue.Queue()                  # Infos des chansons (avec métadonnées)
-q_play = queue.Queue()                   # Infos avec URL audio
-q_data = queue.Queue(maxsize=BUFFER_MAX_SIZE)  # Buffer audio dynamique
-
-# Événements de synchronisation
-stop_event = threading.Event()           # Pour arrêter les threads proprement
-next_event = threading.Event()           # Pour passer à la piste suivante
-buffer_ready_event = threading.Event()   # Pour démarrer la lecture audio lorsque le tampon est prêt
-
-# ✅ Variables Partagées pour l'Interface Graphique
+# ✅ Shared variables for UI
 current_track_info = {
     'titre': 'Titre',
     'artiste': 'Artiste',
@@ -62,38 +50,41 @@ class TactileZone:
     page: str
 
 # Set locale
-locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
+try:
+    locale.setlocale(locale.LC_ALL, "fr_FR.UTF-8")
+except:
+    pass
 
 # ✅ Global variables
 BUFFER_THRESHOLD = 10  # Seuil pour déclencher l'événement de lecture audio
 BUFFER_MAX_SIZE = 50   # Taille maximale du tampon audio
 
 # Global variables
-ydl_opts   = {"geturl": True, "quiet": True}
-tgt_arch   = "aarch64"
-bt_addr    = "41:42:50:E4:3C:4D"
-first_run  = True
-info       = False
-animate    = False
-bt_present = False
-solar_pw   = 0.0
-grid_pw    = 0.0
-batt_pw    = 0.0
-batt_max   = 5000.0
-grid_max   = 6800.0
-sol_max    = 2920.0
-tempo_now  = 'UNKN'
-tempo_tmw  = 'UNKN'
-cur_batt   = '0,0%'
-cur_temp   = '19.0°'
-cur_hum    = '45%'
-ui_page    = 'main'
-gauge_h    = 80
-anim_pct   = 0
-anim_dly   = 5
-audio_cnt  = 0
+ydl_opts         = {"geturl": True, "quiet": True}
+bt_addr          = "41:42:50:E4:3C:4D"
+main_first_run   = True
+sched_first_run  = True
+info             = False
+animate          = False
+bt_present       = False
+solar_pw         = 0.0
+grid_pw          = 0.0
+batt_pw          = 0.0
+batt_max         = 5000.0
+grid_max         = 6800.0
+sol_max          = 2920.0
+tempo_now        = 'UNKN'
+tempo_tmw        = 'UNKN'
+cur_batt         = '0,0%'
+cur_temp         = '19.0°'
+cur_hum          = '45%'
+ui_page          = 'main'
+gauge_h          = 80
+anim_pct         = 0
+anim_dly         = 5
+audio_cnt        = 0
 
-# ✅ Fonction : Sélection de la meilleure miniature
+# ✅ Fonction : Get best thumbnail
 def get_best_thumbnail(thumbnails):
     square_thumbnails = [
         t for t in thumbnails if t.get('width') == t.get('height') and t['width'] <= 160
@@ -107,7 +98,7 @@ def get_best_thumbnail(thumbnails):
     )
     return closest_thumbnail['url']
 
-# ✅ Fonction pour télécharger et redimensionner la miniature
+# ✅ Download & resize thumbnail
 def load_thumbnail(url):
     try:
         response = requests.get(url)
@@ -117,7 +108,7 @@ def load_thumbnail(url):
     except:
         return None
 
-# ✅ Thread 1 : Traitement des playlists
+# ✅ Thread 1 : Manage playlists
 def manage_playlist():
     while not stop_event.is_set():
         try:
@@ -144,7 +135,7 @@ def manage_playlist():
             print(f"Erreur lors du traitement de la playlist {playlist_id} : {e}")
             q_playlist.task_done()
 
-# ✅ Thread 2 : Extraction de l'URL audio
+# ✅ Thread 2 : Extract audio stream URL
 def manage_track():
     ydl_opts = {
         'quiet': True,
@@ -175,8 +166,8 @@ def manage_track():
                 print(f"Erreur lors de l'extraction audio pour {video_id} : {e}")
                 q_track.task_done()
 
-# ✅ Fonction : Lancement de FFmpeg (conversion_audio)
-def conversion_audio(audio_url):
+# ✅ Fonction : Convert audio using ffmpeg
+def convert_audio(audio_url):
     try:
         cmd = [
             'ffmpeg',
@@ -213,13 +204,13 @@ def manage_audio():
             retries = 3
             process = None
             while retries > 0 and process is None:
-                process = conversion_audio(audio_url)
+                process = convert_audio(audio_url)
                 if process is None:
                     retries -= 1
                     time.sleep(2)
 
             if process is None:
-                print(f"Échec de la conversion pour {audio_url}")
+                print(f"Failed to convert {audio_url}")
                 q_play.task_done()
                 continue
 
@@ -240,10 +231,10 @@ def manage_audio():
                         q_data.put(data, timeout=1)
                         buffer_count += 1
                         if buffer_count == BUFFER_THRESHOLD and not buffer_ready_event.is_set():
-                            print("Remplissage cible atteint")
+                            print("Buffer filled")
                             buffer_ready_event.set()
                     except queue.Full:
-                        print("File tampon de données audio pleine")
+                        print("Buffer overflow")
                         continue
 
             q_data.put(None)
@@ -261,7 +252,7 @@ def manage_audio():
         except queue.Empty:
             continue
         except Exception as e:
-            print(f"Erreur lors du traitement audio : {e}")
+            print(f"Audio error : {e}")
             q_play.task_done()
 
 # ✅ Thread 4 : Lecture audio avec PyAudio
@@ -270,16 +261,16 @@ def play_audio():
     stream = p.open(format=pyaudio.paInt16, channels=2, rate=44100, output=True)
 
     while not stop_event.is_set():
-        print("En attente que le tampon soit rempli pour le prochain morceau...")
+        print("Waiting buffer for next track...")
         buffer_ready_event.wait()
         buffer_ready_event.clear()
-        print("Tampon prêt. Début de la lecture audio.")
+        print("Buffer ready, play audio.")
 
         while not stop_event.is_set():
             try:
                 data = q_data.get(timeout=1)
                 if data is None:
-                    print("Fin du morceau détectée.")
+                    print("End of track detected.")
                     q_data.task_done()
                     break
 
@@ -295,6 +286,19 @@ def play_audio():
     stream.stop_stream()
     stream.close()
     p.terminate()
+
+def boiler_relay(cmd = "Query"):
+    if not rpi:
+        return False
+    if cmd == 'Init':
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(9, GPIO.OUT)
+        GPIO.output(9, GPIO.LOW)
+    if cmd == 'OFF':
+        GPIO.output(9, GPIO.LOW)
+    if cmd == 'ON':
+        GPIO.output(9, GPIO.HIGH)
+    return GPIO.input(9)
 
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
@@ -378,7 +382,14 @@ def gaugeDraw(base, pct, col, rev = False):
             n += 1
             gaugeDrawLine(767, y, n, col)
 
-# Redraw full screen function
+# Redraw main screen
+def buildSchedUI():
+    global sched_first_run
+
+    screen.blit(background, (0, 0))
+    sched_first_run = False
+
+# Redraw main screen
 def buildMainUI():
     global info
     global cur_hum
@@ -390,110 +401,120 @@ def buildMainUI():
     global cur_temp
     global tempo_now
     global tempo_tmw
-    global first_run
+    global main_first_run
     global tactile_zones
     global current_track_info
 
+    boiler = boiler_relay()
+    cur_time = strftime('%H:%M')
+    cur_date = strftime('%A ') + strftime('%d').lstrip('0') + strftime(' %B %Y')
+
+    screen.blit(background, (0, 0))
+
+    tempoDraw(tempo_now, (863, 63), 17)
+    tempoDraw(tempo_tmw, (900, 67), 13)
+    r = pygame.Rect(699 - anim_pct, 112, 269 + anim_pct, 432)
+    pygame.draw.rect(screen, fground_col, r, 2, 33)
+    if main_first_run:
+        tactile_zones.append(TactileZone(click_main, r, "info", "main"))
+    if anim_pct != 100:
+        gaugeDraw(216, int(100.0 * solar_pw / sol_max), solar_col)
+        if grid_pw > 0.0:
+            gaugeDraw(327, int( 100.0 * grid_pw / grid_max), grid_fw_col)
+        else:
+            gaugeDraw(327, int(-100.0 * grid_pw / grid_max), grid_bw_col, True)
+        if batt_pw > 0.0:
+            gaugeDraw(438, int( 100.0 * batt_pw / batt_max), bat_chg_col)
+        else:
+            gaugeDraw(438, int(-100.0 * batt_pw / batt_max), bat_dch_col, True)
+    else:
+        pass
+        y = 180
+        for watt in [ solar_pw, grid_pw, batt_pw ]:
+            text_srf = font_batt.render("%dW" % watt, True, fground_col, None)
+            text_crd = text_srf.get_rect()
+            text_crd.midright = (805, y)
+            screen.blit(text_srf, text_crd)
+            y += 111
+
+    date_srf = font_date.render(cur_date, True, fground_col, None)
+    date_crd = date_srf.get_rect()
+    date_crd.center = (295, 150)
+
+    hour_srf = font_hour.render(cur_time, True, fground_col, None)
+    hour_crd = hour_srf.get_rect()
+    hour_crd.center = (300, 300)
+
+    temp_srf = font_temp.render(cur_temp, True, fground_col, None)
+    temp_crd = temp_srf.get_rect()
+    temp_crd.center = (180,  80)
+
+    hum_srf  = font_hum.render (cur_hum , True, fground_col, None)
+    hum_crd  = hum_srf.get_rect()
+    hum_crd.center =  (390,  80)
+
+    batt_srf = font_batt.render(cur_batt, True, fground_col, None)
+    batt_crd = batt_srf.get_rect()
+    batt_crd.center = (830,  485)
+
+    text_srf = font_text.render('Tempo', True, fground_col, None)
+    text_crd = text_srf.get_rect()
+    text_crd.center = (788,   65)
+
+    screen.blit(hour_srf, hour_crd)
+    screen.blit(date_srf, date_crd)
+    screen.blit(temp_srf, temp_crd)
+    screen.blit(hum_srf,  hum_crd)
+    screen.blit(batt_srf, batt_crd)
+    screen.blit(text_srf, text_crd)
+    if boiler:
+        screen.blit(blue_flame, (275, 50))
+    else:
+        screen.blit(grey_flame, (275, 50))
+    screen.blit(icon_solar,     (830, 138))
+    screen.blit(icon_grid,      (830, 251))
+    screen.blit(icon_battery,   (830, 361))
+    if bt_present:
+        if current_track_info.get('playing'):
+            img = pause
+        else:
+            img = play_enabled
+    else:
+        img = play_disabled
+    r = screen.blit(img , (225, 453))
+    if main_first_run:
+        tactile_zones.append(TactileZone(click_main, r, "play", "main"))
+    if bt_present:
+        img = next_enabled
+    else:
+        img = next_disabled
+    r = screen.blit(img,  (322, 453))
+    if main_first_run:
+        tactile_zones.append(TactileZone(click_main, r, "next", "main"))
+    if bt_present:
+        img = bt_enabled
+    else:
+        img = bt_disabled
+    r = screen.blit(img, (427, 453))
+    if main_first_run:
+        tactile_zones.append(TactileZone(click_main, r, "bt", "main"))
+    albumart = current_track_info.get('miniature')
+    if not albumart:
+        albumart = audio_wait
+    r = screen.blit(albumart, (20, 400))
+    if main_first_run:
+        tactile_zones.append(TactileZone(click_main, r, "playlist", "main"))
+    main_first_run = False
+
+def build_ui():
     while not stop_event.is_set():
-        boiler = GPIO.input(9)
-        cur_time = strftime('%H:%M')
-        cur_date = strftime('%A ') + strftime('%d').lstrip('0') + strftime(' %B %Y')
-
-        screen.blit(background, (0, 0))
-
-        tempoDraw(tempo_now, (863, 63), 17)
-        tempoDraw(tempo_tmw, (900, 67), 13)
-        r = pygame.Rect(699 - anim_pct, 112, 269 + anim_pct, 432)
-        pygame.draw.rect(screen, fground_col, r, 2, 33)
-        if first_run:
-            tactile_zones.append(TactileZone(click_main, r, "info", "main"))
-        if anim_pct != 100:
-            gaugeDraw(216, int(100.0 * solar_pw / sol_max), solar_col)
-            if grid_pw > 0.0:
-                gaugeDraw(327, int( 100.0 * grid_pw / grid_max), grid_fw_col)
-            else:
-                gaugeDraw(327, int(-100.0 * grid_pw / grid_max), grid_bw_col, True)
-            if batt_pw > 0.0:
-                gaugeDraw(438, int( 100.0 * batt_pw / batt_max), bat_chg_col)
-            else:
-                gaugeDraw(438, int(-100.0 * batt_pw / batt_max), bat_dch_col, True)
-        else:
-            pass
-            y = 180
-            for watt in [ solar_pw, grid_pw, batt_pw ]:
-                text_srf = font_batt.render("%dW" % watt, True, fground_col, None)
-                text_crd = text_srf.get_rect()
-                text_crd.midright = (805, y)
-                screen.blit(text_srf, text_crd)
-                y += 111
-
-        date_srf = font_date.render(cur_date, True, fground_col, None)
-        date_crd = date_srf.get_rect()
-        date_crd.center = (295, 150)
-
-        hour_srf = font_hour.render(cur_time, True, fground_col, None)
-        hour_crd = hour_srf.get_rect()
-        hour_crd.center = (300, 300)
-
-        temp_srf = font_temp.render(cur_temp, True, fground_col, None)
-        temp_crd = temp_srf.get_rect()
-        temp_crd.center = (180,  80)
-
-        hum_srf  = font_hum.render (cur_hum , True, fground_col, None)
-        hum_crd  = hum_srf.get_rect()
-        hum_crd.center =  (390,  80)
-
-        batt_srf = font_batt.render(cur_batt, True, fground_col, None)
-        batt_crd = batt_srf.get_rect()
-        batt_crd.center = (830,  485)
-
-        text_srf = font_text.render('Tempo', True, fground_col, None)
-        text_crd = text_srf.get_rect()
-        text_crd.center = (788,   65)
-
-        screen.blit(hour_srf, hour_crd)
-        screen.blit(date_srf, date_crd)
-        screen.blit(temp_srf, temp_crd)
-        screen.blit(hum_srf,  hum_crd)
-        screen.blit(batt_srf, batt_crd)
-        screen.blit(text_srf, text_crd)
-        if boiler:
-            screen.blit(blue_flame, (275, 50))
-        else:
-            screen.blit(grey_flame, (275, 50))
-        screen.blit(icon_solar,     (830, 138))
-        screen.blit(icon_grid,      (830, 251))
-        screen.blit(icon_battery,   (830, 361))
-        if bt_present:
-            if current_track_info.get('playing'):
-                img = pause
-            else:
-                img = play_enabled
-        else:
-            img = play_disabled
-        r = screen.blit(img , (225, 453))
-        if first_run:
-            tactile_zones.append(TactileZone(click_main, r, "play", "main"))
-        if bt_present:
-            img = next_enabled
-        else:
-            img = next_disabled
-        r = screen.blit(img,  (322, 453))
-        if first_run:
-            tactile_zones.append(TactileZone(click_main, r, "next", "main"))
-        if bt_present:
-            img = bt_enabled
-        else:
-            img = bt_disabled
-        r = screen.blit(img, (427, 453))
-        if first_run:
-            tactile_zones.append(TactileZone(click_main, r, "bt", "main"))
-        screen.blit(albumart, (20, 400))
-
+        if ui_page == "main":
+            buildMainUI()
+        elif ui_page == "schedule":
+            buildSchedUI()
         pygame.display.flip()
         # limit to 50fps to prevent CPU overload
         clock.tick(50)
-        first_run = False
 
 def tempoUpdate():
     global tempo_now
@@ -522,9 +543,9 @@ def click_main(duration_ms, name):
         match name:
             case "boiler":
                 if boiler:
-                    GPIO.output(9, GPIO.LOW)
+                    boiler_relay('OFF')
                 else:
-                    GPIO.output(9, GPIO.HIGH)
+                    boiler_relay('ON')
                     pygame.time.set_timer(BOILER_OFF, 20*60000, 1)
     else: # short press
         print("Short click on", name)
@@ -540,6 +561,11 @@ def click_main(duration_ms, name):
                     pygame.time.set_timer(ANIMATE, anim_dly, 100)
 
 def manage_events():
+    global bt_present
+    global cur_temp
+    global cur_hum
+    global sensor
+
     while not stop_event.is_set():
         # poll for events
         for event in pygame.event.get():
@@ -567,11 +593,12 @@ def manage_events():
                 tempoUpdate()
             elif event.type == TEMP_TICK:
                 bt_present = bt_addr in sp.getoutput("hcitool con").split()
-                buf = "%0.1f°" % sensor.temperature
-                cur_temp = buf.replace(".", ",")
-                cur_hum  = "%d%%" % int(sensor.relative_humidity)
+                if sensor:
+                    buf = "%0.1f°" % sensor.temperature
+                    cur_temp = buf.replace(".", ",")
+                    cur_hum  = "%d%%" % int(sensor.relative_humidity)
             elif event.type == BOILER_OFF:
-                GPIO.output(9, GPIO.LOW)
+                boiler_relay('OFF')
             elif event.type == INFO_OFF:
                 info = False
                 pygame.time.set_timer(ANIMATE, anim_dly, 100)
@@ -583,20 +610,35 @@ def manage_events():
             else:
                 print("Unknown event %d", event.type)
 
+# ✅ YTMusic initialisation with OAuth
+ytmusic = YTMusic('.priv/oauth.json')
+
+# ✅ Files tampons
+q_playlist = queue.Queue()               # IDs des playlists
+q_track = queue.Queue()                  # Infos des chansons (avec métadonnées)
+q_play = queue.Queue()                   # Infos avec URL audio
+q_data = queue.Queue(maxsize=BUFFER_MAX_SIZE)  # Buffer audio dynamique
+
+# Sync events
+stop_event = threading.Event()           # Pour arrêter les threads proprement
+next_event = threading.Event()           # Pour passer à la piste suivante
+buffer_ready_event = threading.Event()   # Pour démarrer la lecture audio lorsque le tampon est prêt
+
 # GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(9, GPIO.OUT)
-GPIO.output(9, GPIO.LOW)
+boiler_relay('Init')
 
 # Temperature sensor setup
-i2c = board.I2C()  # uses board.SCL and board.SDA
-sensor = HTU21D(i2c)
+if rpi:
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    sensor = HTU21D(i2c)
+else:
+    sensor = None
 
 # pygame setup
 pygame.init()
 pygame.font.init()
 pygame.mouse.set_visible(False)
-if (platform.machine() == tgt_arch):
+if rpi:
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 else:
     screen = pygame.display.set_mode((1024, 600))
@@ -626,6 +668,7 @@ icon_solar    = pygame.image.load('images/icon-solar.png')
 play_disabled = pygame.image.load('images/play-disabled.png')
 play_enabled  = pygame.image.load('images/play-enabled.png')
 pause         = pygame.image.load('images/pause.png')
+audio_wait    = pygame.image.load('images/audio_wait.jpg')
 next_disabled = pygame.image.load('images/next-disabled.png')
 next_enabled  = pygame.image.load('images/next-enabled.png')
 bt_disabled   = pygame.image.load('images/bt-disabled.png')
@@ -684,7 +727,7 @@ thread_playlist = threading.Thread(target=manage_playlist)
 thread_track = threading.Thread(target=manage_track)
 thread_audio = threading.Thread(target=manage_audio)
 thread_play = threading.Thread(target=play_audio)
-thread_gui = threading.Thread(target=buildMainUI)
+thread_gui = threading.Thread(target=build_ui)
 thread_evts = threading.Thread(target=manage_events)
 
 thread_playlist.start()
