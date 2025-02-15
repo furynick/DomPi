bc59c8ea1f61c41ea5b566ebb133163ee477320d
Nicolas T <furydonnico@gmail.com>
2025-02-15 17:32:58 +0100

    Fix exec right

--- install/upload.sh
+++ install/upload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+cd "$(dirname $(readlink -f $0))"
 read -p "WiFi SSID: " SSID
 read -p "WiFi Pass: " PASS
-curl https://dietpi.com/downloads/images/DietPi_RPi234-ARMv8-Bookworm.img.xz | unxz | sudo dd of=/dev/sda bs=4M
+T=$'\t'
+curl https://dietpi.com/downloads/images/DietPi_RPi234-ARMv8-Bookworm.img.xz | unxz | sudo dd of=/dev/sda bs=4M status=none
 sudo mount /dev/sda1 /mnt
 sudo cp dietpi.txt Automation_Custom_Script.sh /mnt
-sudo sed -i "/WIFI_SSID.0/s²''²'$SSID'²;/WIFI_KEY.0/s²''²'$PASS'²" /mnt/dietpi-wifi.txt
+sudo sed -i "/WIFI_SSID.0/s${T}''${T}'${SSID}'${T};/WIFI_KEY.0/s${T}''${T}'${PASS}'${T}" /mnt/dietpi-wifi.txt
 sudo umount /mnt
