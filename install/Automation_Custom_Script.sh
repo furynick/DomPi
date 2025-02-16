037aa0ad8e411506e4b0e3bc21013b9a88bdad0f
Nicolas T <furydonnico@gmail.com>
2025-02-16 08:41:15 +0100

    Fix upload process

--- install/Automation_Custom_Script.sh
+++ install/Automation_Custom_Script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-su - dietpi -c 'git clone https://github.com/furynick/DomPi.git'
-su - dietpi -c 'DomPi/install/setup.sh'
+SELF="$(readlink -f $0)"
+BASE="${SELF%/*}"
+systemctl disable --now getty@tty1.service
+su - dietpi -c '[ -d .ssh ] || mkdir .ssh; git clone https://github.com/furynick/DomPi.git &&  DomPi/install/setup.sh'
+install -o dietpi -m 0400 $BASE/id_ed25519 ~dietpi/.ssh
+install -o dietpi -m 0644 $BASE/id_ed25519.pub ~dietpi/.ssh
--- install/upload.sh
+++ install/upload.sh
@@ -5,6 +5,6 @@ read -p "WiFi Pass: " PASS
 T=$'\t'
 curl https://dietpi.com/downloads/images/DietPi_RPi234-ARMv8-Bookworm.img.xz | unxz | sudo dd of=/dev/sda bs=4M status=none
 sudo mount /dev/sda1 /mnt
-sudo cp dietpi.txt Automation_Custom_Script.sh /mnt
+sudo cp dietpi.txt Automation_Custom_Script.sh ~/.ssh/id_ed25519* /mnt
 sudo sed -i "/WIFI_SSID.0/s${T}''${T}'${SSID}'${T};/WIFI_KEY.0/s${T}''${T}'${PASS}'${T}" /mnt/dietpi-wifi.txt
 sudo umount /mnt
