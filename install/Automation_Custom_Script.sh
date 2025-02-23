60dd0998973f9add829b5971a63317b07857a281
Nico T <furydonnico@gmail.com>
2025-02-23 11:18:54 +0100

    SDL compilation

--- install/Automation_Custom_Script.sh
+++ install/Automation_Custom_Script.sh
@@ -10,12 +10,13 @@ apt -y install bluez-firmware bluez-alsa-utils alsa-utils dbus bluez
 echo 'SUBSYSTEM=="bluetooth", ACTION=="add", KERNEL=="hci0:*", RUN+="/usr/bin/bluetoothctl connect $env{DEVNAME}"' >/etc/udev/rules.d/99-btconnect.rules 
 
 # Sofware setup
+install -o ${KIOSK_USER} -g ${KIOSK_USER} -m 0755 -d ~${KIOSK_USER}/.ssh
 install -o ${KIOSK_USER} -g ${KIOSK_USER} -m 0400 $BASE/id_ed25519 ~${KIOSK_USER}/.ssh
 install -o ${KIOSK_USER} -g ${KIOSK_USER} -m 0644 $BASE/id_ed25519.pub ~${KIOSK_USER}/.ssh
 usermod -aG audio,tty,gpio,i2c,video ${KIOSK_USER}
 setcap 'cap_net_raw,cap_net_admin+eip' $(readlink -f $(which python3))
-apt -y install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libsdl2-gfx-1.0-0 libts0 libportaudio2 python3-venv
-su - ${KIOSK_USER} -c '[ -d .ssh ] || mkdir .ssh; git clone https://github.com/furynick/DomPi.git &&  DomPi/install/setup.sh'
+apt -y install libts0 libportaudio2 python3-venv
+su - ${KIOSK_USER} -c 'DomPi/install/setup.sh'
 cp ${KIOSK_USER}/DomPi/install/pointercal /etc
 
 # Clean
--- install/setup.sh
+++ install/setup.sh
@@ -1,10 +1,30 @@
 #!/bin/bash -e
-cd $HOME/DomPi
-sudo apt -qq -y install build-essential python3-dev portaudio19-dev
-[ -d .venv ] && rm -rf .venv
-python3 -m venv .venv
-source .venv/bin/activate
-pip install wheel
-pip install -r install/requirements.txt
-sudo apt -qq -y purge build-essential python3-dev portaudio19-dev
+cd $HOME
+DEV_PKGS="build-essential libdrm-dev libgbm-dev libudev-dev libfreetype6-dev"
+DEV_PKGS+=" libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev"
+DEV_PKGS+=" libts-dev python3-dev portaudio19-dev"
+sudo apt -qq -y install $DEV_PKGS
+(
+  git clone https://github.com/libsdl-org/SDL.git
+  cd SDL
+  mkdir build
+  cd build
+  ../configure --enable-video-kmsdrm --enable-video-fbcon --enable-video-directfb \
+             --enable-tff --enable-gfx --enable-image --enable-touchscreen
+  make -j4
+  sudo make install
+)
+
+(
+  git clone https://github.com/furynick/DomPi.git
+  cd DomPi
+  [ -d .venv ] && rm -rf .venv
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip install wheel
+  pip install -r install/requirements.txt
+)
+
+# Clean
+sudo apt -qq -y purge $DEV_PKGS
 sudo apt -qq -y autoremove --purge
