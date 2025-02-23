6298099b551f1081826e2fbc085b7fec93c672f9
Nico T <furydonnico@gmail.com>
2025-02-23 09:23:41 +0100

    Automate bluetooth connection & improve software setup

--- install/Automation_Custom_Script.sh
+++ install/Automation_Custom_Script.sh
@@ -5,22 +5,22 @@ exec 2>&1
 KIOSK_USER=dietpi
 BASE="/boot/firmware"
 
+# Bluetooth setup
+apt -y install bluez-firmware bluez-alsa-utils alsa-utils dbus bluez
+echo 'SUBSYSTEM=="bluetooth", ACTION=="add", KERNEL=="hci0:*", RUN+="/usr/bin/bluetoothctl connect $env{DEVNAME}"' >/etc/udev/rules.d/99-btconnect.rules 
+
 # Sofware setup
 install -o ${KIOSK_USER} -g ${KIOSK_USER} -m 0400 $BASE/id_ed25519 ~${KIOSK_USER}/.ssh
 install -o ${KIOSK_USER} -g ${KIOSK_USER} -m 0644 $BASE/id_ed25519.pub ~${KIOSK_USER}/.ssh
 usermod -aG audio,tty ${KIOSK_USER}
 setcap 'cap_net_raw,cap_net_admin+eip' $(readlink -f $(which python3))
-sed -i 's/+console//' /usr/lib/systemd/system/rc-local.service.d/debian.conf
-sed -i 's/=tty1/=tty5/;s/$/ loglevel=1 vt.global_cursor_default=0 consoleblank=0 quiet/' /boot/firmware/cmdline.txt
-apt -y install build-essential python3-dev portaudio19-dev
-apt -y install bluez-firmware bluez-alsa-utils alsa-utils dbus bluez
 apt -y install libsdl2-2.0-0 libsdl2-ttf-2.0-0 libsdl2-image-2.0-0 libsdl2-gfx-1.0-0 libts0 libportaudio2 python3-venv
 su - ${KIOSK_USER} -c '[ -d .ssh ] || mkdir .ssh; git clone https://github.com/furynick/DomPi.git &&  DomPi/install/setup.sh'
 cp ${KIOSK_USER}/DomPi/install/pointercal /etc
 
 # Clean
-apt -y purge build-essential python3-dev portaudio19-dev
-apt -y autoremove --purge
+sed -i 's/+console//' /usr/lib/systemd/system/rc-local.service.d/debian.conf
+sed -i 's/=tty1/=tty5/;s/$/ loglevel=1 vt.global_cursor_default=0 consoleblank=0 quiet/' /boot/firmware/cmdline.txt
 rm -f $BASE/id_ed25519*
 
 # Service install
--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 cd $HOME/DomPi
+sudo apt -qq -y install build-essential python3-dev portaudio19-dev
+[ -d .venv ] && rm -rf .venv
 python3 -m venv .venv
 source .venv/bin/activate
 pip install wheel
 pip install -r install/requirements.txt
+sudo apt -qq -y purge build-essential python3-dev portaudio19-dev
+sudo apt -qq -y autoremove --purge
