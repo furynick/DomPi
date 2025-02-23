b4088baebc7afff859f396eca6a07a51435dc21a
Nicolas T. <nicolas@furyweb.fr>
2025-02-23 16:27:58 +0100

    Fix setup.sh

--- install/setup.sh
+++ install/setup.sh
@@ -5,12 +5,16 @@ cd $HOME
 DEV_PKGS="build-essential libdrm-dev libgbm-dev libudev-dev libfreetype6-dev"
 DEV_PKGS+=" libts-dev python3-dev portaudio19-dev"
 sudo apt -qq -y install $DEV_PKGS
+git config --global http.version HTTP/1.1
+git config --global http.postBuffer 268435456
+git config --global core.compression 0
 (
-  git clone https://github.com/libsdl-org/SDL.git
-  V=$(git tag | sort | awk '/release-2/ {V=$0} END {print V}')
-  git checkout $V
+  git clone  --depth 1 https://github.com/libsdl-org/SDL.git
   mkdir SDL/build
   cd SDL/build
+  V=$(git tag | sort | awk '/release-2/ {V=$0} END {print V}')
+  git checkout $V
+  git fetch --unshallow
   ../configure --enable-video-kmsdrm --enable-video-fbcon --enable-video-directfb \
              --enable-tff --enable-gfx --enable-image --enable-touchscreen
   make -j4
